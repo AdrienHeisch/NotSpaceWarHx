6e330d4040c8696923e669a097405b90e0fb8abf
AdrienHeisch <adrienheisch@orange.fr>
2018-04-25 22:27:33 +0200

    add haxe project

---
+++ src/com/adrienheisch/notspacewar/Main.hx
@@ -0,0 +1,21 @@
+package com.adrienheisch.notspacewar;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author Adrien Heisch
+ */
+class Main extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+
+}
